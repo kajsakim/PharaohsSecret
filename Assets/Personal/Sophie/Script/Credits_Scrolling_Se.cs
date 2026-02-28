5ebcb9a3e39662638da9e32e7b414e0ff7d655e4
Sophiechan02 <sophie.estelle22@outlook.com>
2026-02-28 22:43:06 +0100

    Credits
    - Credits for the game without the text in it
    
    - automatic scrolling and timer that only needs to be changed from its current number to the time amount it takes for the credits to finish.

---
+++ Assets/Personal/Sophie/Script/Credits_Scrolling_Se.cs
@@ -0,0 +1,27 @@
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Credits_Scrolling_Se : MonoBehaviour
+{
+    public float Scroll_Speed = 32.0f; // the speed that the text moves at
+    public float Scroll_Timer = 6.0f; // the duration of the timer that will send the player back to the main menu.
+
+    private RectTransform Rect_Transform;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Rect_Transform = GetComponent<RectTransform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Scroll_Timer -= Time.deltaTime;
+        Rect_Transform.anchoredPosition += new Vector2(0, Scroll_Speed * Time.deltaTime);
+        
+        if (Scroll_Timer <= 0)
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+}
---
+++ Assets/Personal/Sophie/Script/Menu_Manager_Se.cs
@@ -0,0 +1,34 @@
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Menu_Manager_Se : MonoBehaviour
+{
+    public void PlaySceneGame ()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void PlaySceneHowToPlay ()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void PlaySceneCredits ()
+    {
+        SceneManager.LoadScene(4);
+    }
+    public void ExitGame ()
+    {
+        Application.Quit();
+    }
+
+    public void PlaySceneMainMenu ()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void PlaySceneReload ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+}
