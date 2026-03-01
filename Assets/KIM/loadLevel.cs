969155144ee5055d9b803e0d0727a7ac33210950
kajsakim <140954836+kajsakim@users.noreply.github.com>
2026-03-01 12:06:57 +0100

    fixed scene load to memory from entry room

--- Assets/KIM/loadLevel.cs
+++ Assets/KIM/loadLevel.cs
@@ -4,17 +4,25 @@ using UnityEngine.SceneManagement;
 
 public class loadLevel : MonoBehaviour
 {
-    public GameObject levelTrigger;
+    public BoxCollider2D levelTrigger;
     public NPC NPC;
     public GameObject enterPopup;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    public void OnTriggerEnter2D(Collider2D collision)
+    public void OnTriggerStay2D(Collider2D collision)
     {
         enterPopup.SetActive(true);
-        if (NPC.isDialogDone && Input.GetKeyUp(KeyCode.E))
+        if (Input.GetKey(KeyCode.E))
         {
-            SceneManager.LoadSceneAsync(3);//this aint working, dunno why yet.
+            if (NPC.isDialogDone)
+            {
+                SceneManager.LoadSceneAsync("memoryPuzzle_kk");
+            }
+            //if (Input.GetKey(KeyCode.E))
+            //{
+                
+                //SceneManager.LoadSceneAsync("memoryPuzzle_kk");//this aint working, dunno why yet.
+            //}
         }
     }
     public void OnTriggerExit2D(Collider2D collision)
